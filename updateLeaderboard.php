24751b8d0bdcf691ca9e6c39bb892c4fbeaae10d
Rózsa Balázs <h985122@stud.u-szeged.hu>
2022-01-07 12:28:15 +0100

    deleted old leaderboard

--- updateLeaderboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-//$data = isset($_POST['data']) ? json_decode($_POST['data']): new \stdClass();
-$strRequest = file_get_contents('php://input');
-
-$jsonFile = fopen("assets/leaderboard.json", "w") or die("Unable to open file!");
-fwrite($jsonFile, $strRequest);
-fclose($jsonFile);
-?>
